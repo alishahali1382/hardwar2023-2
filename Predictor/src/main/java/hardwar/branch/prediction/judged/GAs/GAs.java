a1129490fc2d37536266feac6cdc7fec5786135b
Arefe Boushehrian <arefeboushehrian@Arefes-MacBook-Pro.local>
2023-06-01 06:32:38 -0700

    GAs

--- Predictor/src/main/java/hardwar/branch/prediction/judged/GAs/GAs.java
+++ Predictor/src/main/java/hardwar/branch/prediction/judged/GAs/GAs.java
@@ -29,19 +29,19 @@ public class GAs implements BranchPredictor {
      */
     public GAs(int BHRSize, int SCSize, int branchInstructionSize, int KSize, HashMode hashmode) {
         // TODO: complete the constructor
-        this.branchInstructionSize = 0;
-        this.KSize = 0;
-        this.hashMode = HashMode.XOR;
+        this.branchInstructionSize = branchInstructionSize;
+        this.KSize = KSize;
+        this.hashMode = hashmode;
 
         // Initialize the BHR register with the given size and no default value
-        BHR = null;
+        BHR = new SIPORegister("BHR", BHRSize, null);
 
         // Initializing the PAPHT with K bit as PHT selector and 2^BHRSize row as each PHT entries
         // number and SCSize as block size
-        PSPHT = null;
+        PSPHT = new PerAddressPredictionHistoryTable(KSize, 1 << BHRSize, SCSize);
 
         // Initialize the saturating counter
-        SC = null;
+        SC = new SIPORegister("SC", SCSize, null);
     }
 
     /**
@@ -54,7 +54,9 @@ public class GAs implements BranchPredictor {
     @Override
     public BranchResult predict(BranchInstruction branchInstruction) {
         // TODO: complete Task 1
-        return BranchResult.NOT_TAKEN;
+        Bit [] a = getCacheEntry(CombinationalLogic.hash(branchInstruction.getInstructionAddress(), KSize, hashMode));
+        SC.load(PSPHT.setDefault(a, getDefaultBlock()));
+        return BranchResult.of(SC.read()[0].getValue());
     }
 
     /**
@@ -66,6 +68,9 @@ public class GAs implements BranchPredictor {
     @Override
     public void update(BranchInstruction branchInstruction, BranchResult actual) {
         // TODO: complete Task 2
+        Bit[] newData = CombinationalLogic.count(SC.read(), BranchResult.isTaken(actual), CountMode.SATURATING);
+        PSPHT.put(getCacheEntry(CombinationalLogic.hash(branchInstruction.getInstructionAddress(), KSize, hashMode)), newData);
+        BHR.insert(Bit.of(BranchResult.isTaken(actual)));
     }
 
     /**
